006c609649cd07a76cf13c4fe93f569b177725fa
Accel Castillon <castillonaccel26@gmail.com>
2026-03-24 12:45:42 +0800

    push

--- payroll/vite.config.js
+++ payroll/vite.config.js
@@ -4,14 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: process.env.VITE_BASE_PATH || "/react-vite-taskmanager",
+  build: {
+    outDir: 'dist',
+  },
+  base: './',
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
+        target: "http://localhost:8000",
         changeOrigin: true,
-      },
-    },
-  },
-});
+      }
+    }
+  }
+})
